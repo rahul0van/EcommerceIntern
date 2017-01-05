152676cbbc319fb2d1c61b5e7efbbdaa1d7d09fa
rahul0van <rahul0van@gmail.com>
2017-01-05 23:05:55 +0545

    second update

--- resources/views/productdetail.blade.php
+++ resources/views/productdetail.blade.php
@@ -11,14 +11,21 @@
         <h3>Brand: {{ $attribute->brand}}</h3><br>
         <h3>Description: {{ $attribute->description}}</h3><br>
 
-    @endforeach
-    {!! Form::open(array('route' => 'payment.create','method'=>'get')) !!}
-    <div class="form-group">
-        <label class="col-md-4 control-label" for="selectedquantity"><h3>Select Quantity</h3></label>
-    <div class="col-md-4">
-        <input id="selectedquantity" name="selectedquantity"  class="form-control input-md"  type="text">
 
-    </div></div>
+{{--    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+        <!-- Identify your business so that you can collect the payments. -->
+        <h3>Paypal Email: </h3> <input type="text" name="business" class="form-control input-md" ><br>
+
+        <!-- Specify a PayPal Shopping Cart Add to Cart button. -->
+        <input type="hidden" name="cmd" value="_cart">
+        <input type="hidden" name="add" value="1">
+
+        <!-- Specify details about the item that buyers will purchase. -->
+        <input type="hidden" name="item_name" value="{{$attribute->productname}}">
+        <input type="hidden" name="amount" value="{{ $attribute->price}}">
+        <input type="hidden" name="currency_code" value="USD">
+    <h3>Select Quantity</h3><input id="quantity" name="quantity"  class="form-control input-md"  type="text">
+
 
 
     <div class="form-group">
@@ -26,6 +33,17 @@
             <button id="singlebutton" name="singlebutton" class="btn btn-primary">Buy Now</button>
         </div>
     </div>
-    {!! Form::close() !!}
+    </form>--}}
+        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_s-xclick">
+            <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
+            <input type="image"
+                   src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now">
+            <img alt="" src="https://paypalobjects.com/en_US/i/scr/pixel.gif"
+                 width="1" height="1">
+        </form>
+
+
+    @endforeach
 
 @endsection
