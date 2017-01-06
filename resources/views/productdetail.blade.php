f522f6918904f3e64770099fd4975845df753a26
rahul0van <rahul0van@gmail.com>
2017-01-06 20:02:12 +0545

    3rd update

--- resources/views/productdetail.blade.php
+++ resources/views/productdetail.blade.php
@@ -12,14 +12,15 @@
         <h3>Description: {{ $attribute->description}}</h3><br>
 
 
-{{--    <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+  <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
         <!-- Identify your business so that you can collect the payments. -->
+      <input type="hidden" name="cmd" value="_xclick">
         <h3>Paypal Email: </h3> <input type="text" name="business" class="form-control input-md" ><br>
 
         <!-- Specify a PayPal Shopping Cart Add to Cart button. -->
-        <input type="hidden" name="cmd" value="_cart">
+       {{-- <input type="hidden" name="cmd" value="_cart">
         <input type="hidden" name="add" value="1">
-
+--}}
         <!-- Specify details about the item that buyers will purchase. -->
         <input type="hidden" name="item_name" value="{{$attribute->productname}}">
         <input type="hidden" name="amount" value="{{ $attribute->price}}">
@@ -33,15 +34,16 @@
             <button id="singlebutton" name="singlebutton" class="btn btn-primary">Buy Now</button>
         </div>
     </div>
-    </form>--}}
-        <form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
-            <input type="hidden" name="cmd" value="_s-xclick">
+    </form>
+        {{--<form action="https://www.sandbox.paypal.com/cgi-bin/webscr" method="post">
+            <input type="hidden" name="cmd" value="_xclick">
+            <INPUT TYPE="hidden" name="cmd" value="_xclick-auto-billing">
             <input type="hidden" name="hosted_button_id" value="6RNT8A4HBBJRE">
             <input type="image"
                    src="https://www.paypalobjects.com/webstatic/en_US/i/btn/png/btn_buynow_107x26.png" alt="Buy Now">
             <img alt="" src="https://paypalobjects.com/en_US/i/scr/pixel.gif"
                  width="1" height="1">
-        </form>
+        </form>--}}
 
 
     @endforeach
